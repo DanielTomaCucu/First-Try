b66a35faae4f8707bdf48247cf7091cdea301bbe
DanielTomaCucu <61708051+DanielTomaCucu@users.noreply.github.com>
2020-03-02 20:34:28 +0200

    New Weather app

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    
+    $('sumbit').click(function(){
+        var oras= $('#oras').val();
+        if(oras != ' '){
+            $.ajax({
+                url:'http://api.openweathermap.org/data/2.5/weather?q='+ oras + '&&units=metric'+ '&APPID=eef9c248198c24add1ea29b6518865dc',
+                type:"GET",
+                dataType:"jsonp",
+                success:function(data){
+                    console.log(data);
+                }
+                
+            });
+        }
+        else{
+            $('#eroare').html('Trebuie sa alegryi un oras');
+        }
+    });
+     
+});
