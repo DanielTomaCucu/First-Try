d42228b4f7686cab4200e36cdfdd7de694a80c91
DanielTomaCucu <61708051+DanielTomaCucu@users.noreply.github.com>
2020-03-02 20:37:35 +0200

    added https to links

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         var oras= $('#oras').val();
         if(oras != ' '){
             $.ajax({
-                url:'http://api.openweathermap.org/data/2.5/weather?q='+ oras + '&&units=metric'+ '&APPID=eef9c248198c24add1ea29b6518865dc',
+                url:'https://api.openweathermap.org/data/2.5/weather?q='+ oras + '&&units=metric'+ '&APPID=eef9c248198c24add1ea29b6518865dc',
                 type:"GET",
                 dataType:"jsonp",
                 success:function(data){
